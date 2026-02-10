8ab5ae48fb010939be14b9ae081a8eec7f07100e
MatiasJaramillo <matjaramillo2002@gmail.com>
2026-02-10 00:43:10 -0500

    incluir nueva estructura de proyecto y toda la parte de rest-app completa

---
+++ rest-app/src/main/java/usfq/rest/HelloResource.java
@@ -0,0 +1,19 @@
+package usfq.rest;
+
+import jakarta.annotation.security.PermitAll;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+@PermitAll
+public class HelloResource {
+
+    @GET
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello World";
+    }
+}
